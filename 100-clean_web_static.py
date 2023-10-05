b821f30ef87ea24d243c3c577a70f12ec054bd1c
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-10-05 15:12:03 +0000

    100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -57,6 +57,16 @@ def deploy():
 
 def do_clean(number=0):
     ''' Removes out of date archives '''
-    number = int(number)
-    if number == 0:
-        number = 2
+
+    number = 1 if int(number) == 0 else int(number)
+
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for fd in archives]
+
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [fd for fd in archives if "web_static_" in fd]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for fd in archives]
