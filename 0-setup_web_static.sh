731d0d922e6d4424f09575192e06508297b7475f
Gabriel Wachira <gabbyk.com@gmail.com>
2023-10-06 18:50:52 +0300

    Update configuration file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -19,24 +19,38 @@ echo "Hello World!" | sudo tee /var/www/html/index.html
 
 # Configure redirection for /redirect_me
 # shellcheck disable=SC1004
-sudo sed -i '/server_name _;/a \
-        location /redirect_me {\n\
-                rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;\n\
-        }\n' /etc/nginx/sites-available/default
+
+if grep -q "location /redirect_me" /etc/nginx/sites-available/default; then
+	echo "skipping.. redirect_me exits"
+else
+	sudo sed -i '/server_name _;/a \
+	\nlocation /redirect_me {\n\
+	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;\n\
+	}\n' /etc/nginx/sites-available/default
+fi
 
 # Create a custom 404 HTML page
-echo "Ceci n'est pas une page" | sudo tee /var/www/html/custom_404.html
+sudo echo "Ceci n'est pas une page" | sudo tee /var/www/html/custom_404.html
 
 # Modify the Nginx configuration to use the custom 404 page
 # shellcheck disable=SC1004
-sudo sed -i '/server_name _;/a \
-        error_page 404 /custom_404.html;\n\
-        location = /custom_404.html {\n\
-                internal;\n\
-        }\n' /etc/nginx/sites-available/default
+
+if grep -q "error_page 404 /custom_404.html" /etc/nginx/sites-available/default; then
+        echo "skipping... custom 404 exist"
+else
+	sudo sed -i '/server_name _;/a \
+	\nerror_page 404 /custom_404.html;\n\
+	location /custom_404.html {\n\
+	internal;\n\
+	}\n' /etc/nginx/sites-available/default
+fi
 
 # Configure custom HTTP response header
-sudo echo 'add_header X-Served-By $hostname;' | sudo tee /etc/nginx/conf.d/0-custom-header.conf
+if grep -q "add_header X-Served-By" /etc/nginx/conf.d/0-custom-header.conf; then
+	echo "skipping... x-served-by header exists"
+else
+	sudo echo 'add_header X-Served-By $hostname;' | sudo tee /etc/nginx/conf.d/0-custom-header.conf
+fi
 
 #------------------------------------------------------ end of section
 
@@ -51,9 +65,13 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu: /data/
 
 # shellcheck disable=SC1004
-sudo sed -i '/server_name _;/a \
-	     location = /hbnb_static {\n\
-		     alias data//web_static/current;\n\
-	     }\n' /etc/nginx/sites-available/default
+if grep -q "location /hbnb_static" /etc/nginx/sites-available/default; then
+	echo "skipping...hbnb_static exitst"
+else
+	sudo sed -i '/server_name _;/a \
+	\n\nlocation /hbnb_static {\n\
+	alias /data/web_static/current/;\n\
+	}\n' /etc/nginx/sites-available/default
+fi
 
 sudo service nginx restart
