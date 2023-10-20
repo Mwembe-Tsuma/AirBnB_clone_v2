242607308aff3e2a3a01d34890d7b7166378bff6
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-10-20 11:56:56 +0000

    1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """"script that starts a Flask web application"""
 
-
 from flask import Flask
 
 
@@ -12,9 +11,11 @@ app = Flask(__name__)
 def hello_hbnb():
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb')
 def hbnb():
     return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
