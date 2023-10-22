5de08b9d29a51dd0e768d4a6209d9ad2582bccfe
Mwembe-Tsuma <49154717+Mwembe-Tsuma@users.noreply.github.com>
2023-10-22 15:15:54 +0300

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,26 +1,24 @@
 #!/usr/bin/python3
 """"script that starts a Flask web application"""
 
-from flask import Flask, render_template
+from web_flask import app
+from flask import render_template
 from models import storage
 from models.state import State
-
-
-app = Flask(__name__)
-app.url_map.strict_slashes = False
-
-
-@app.route('/states_list')
-def states_list():
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
 def teardown(exception):
     storage.close()
+    if exception:
+        print(exception)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    all_states = storage.all(State)
+    return render_template('7-states_list.html', all_states=all_states)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
