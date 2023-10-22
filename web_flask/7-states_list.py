e9a2a6093276b6a0d589c5ba5d28cdfc09555af9
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-10-22 09:39:11 +0000

    7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,8 +12,8 @@ app.url_map.strict_slashes = False
 
 @app.route('/states_list')
 def states_list():
-    states = list(storage.all("State").values())
-    states.sort(key=lambda state: state.name)
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda state: state.name)
     return render_template('7-states_list.html', states=states)
 
 
@@ -23,4 +23,4 @@ def teardown(exception):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True) 
