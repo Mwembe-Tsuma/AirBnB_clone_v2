6ebdccbdb7bc7f324dac5378068b9a84da6ff8df
Mwembe-Tsuma <49154717+Mwembe-Tsuma@users.noreply.github.com>
2023-10-22 15:26:36 +0300

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -15,7 +15,7 @@ def teardown(exception):
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    all_states = storage.all(State)
+    states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
 
