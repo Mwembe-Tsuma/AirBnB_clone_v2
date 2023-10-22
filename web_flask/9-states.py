6380de2261b062dc19be5c3deed72f826cc6347f
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-10-22 15:14:30 +0000

    9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -5,37 +5,44 @@ from os import getenv
 from models import storage
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/states')
-def states_list():
-    current_id = "0"
+@app.route("/states", strict_slashes=False)
+def states():
+    """
+    The function retrieves a list of all states from storage.
+    """
+    with_id = "0"
     all_states = storage.all(State)
     states = sorted(all_states.values(), key=lambda state: state.name)
-    return render_template("9-states.html",
-                           states=states, current_id=current_id)
+    return render_template("9-states.html", states=states, with_id=with_id)
 
 
-@app.route('/states/<id>')
-def state_cities(id):
-    current_id = "1"
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id):
+    """
+    The function `states_id` retrieves a state object.
+    """
+    with_id = "1"
     all_state = storage.all(State)
     key = "State" + "." + str(id)
     try:
         state = all_state[key]
     except KeyError:
-        curr_id = "2"
-        return render_template("9-states.html", curr_id=curr_id)
+        width_id = "2"
+        return render_template("9-states.html", width_id=width_id)
     storage_type = getenv("HBNB_TYPE_STORAGE")
     return render_template("9-states.html", state=state,
-                           storage_type=storage_type, current_id=current_id)
+                           storage_type=storage_type, with_id=with_id)
 
 
 @app.teardown_appcontext
-def close_db_session(exception):
+def close(exception):
+    """
+    close a storage object.
+    """
     storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
